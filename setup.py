1fb30d6b2cc27ec551e16f472dee4195ac74badf
Travis Paquette <tpaqu15@gmail.com>
2021-01-13 21:44:34 -0700

    Refactor current code for packaging. (#1)
    * Refactor current code for packaging.
    
    - Change the name of package to yfantasy-api
    - Use absolute imports
    - Add actions for linting and publishing the package
    - Set the version as part of the publish action
    - Run flake8 on MR and master builds
    - Add a requirements.txt file

--- setup.py
+++ setup.py
@@ -3,16 +3,20 @@ import setuptools
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name='yahoo-api-hkyplyr',
-    version='0.0.2',
-    author='Travis Paquette',
-    author_email='tpaqu15@gmail.com',
-    description='Python app to access the Yahoo! Fantasy APIs',
-    long_description=long_description,
-    long_description_content_Type='text/markdown',
-    url='https://github.com/hkyplyr/yahoo-api',
-    packages=setuptools.find_packages(),
-    classifiers=[],
-    python_requires='>=3.6',
-)
+
+if __name__ == "__main__":
+    import yfantasy_api
+
+    setuptools.setup(
+        name='yfantasy-api',
+        version=yfantasy_api.__version__,
+        author='Travis Paquette',
+        author_email='tpaqu15@gmail.com',
+        description='Python package to access the Yahoo! Fantasy APIs',
+        long_description=long_description,
+        long_description_content_Type='text/markdown',
+        url='https://github.com/hkyplyr/yfantasy-api',
+        packages=setuptools.find_packages(),
+        classifiers=[],
+        python_requires='>=3.6',
+    )
